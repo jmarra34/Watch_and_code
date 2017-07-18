0610aaf5f36ba6ba2f47bb3768cac04691beedd3
Justin <justinmarrapese@gmail.com>
2017-07-18 00:54:05 +0000

    2nd commit

---
+++ todosA.js
@@ -0,0 +1,21 @@
+var newValue;
+
+var todoList = {
+    todos: ['item 1', 'item 2', 'item 3'],
+    displayTodos: function() {
+        console.log('My Todos', this.todos)
+    },
+    addTodo: function(todo){
+        this.todos.push(todo);
+        this.displayTodos();
+    },
+    changeTodo: function(position, NewValue) {
+        this.todos[position] = newValue;
+        this.displayTodos();
+    },
+    deleteTodo: function(position) {
+        this.todos.splice(position,1);
+        this.displayTodos();
+    }
+};
+
